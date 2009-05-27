9b042f17751ab74e618a7fd747778f47f2148431
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2009-05-27 00:22:04 +0000

    first version: basic XMLisp with scene graph, picking animation

---
+++ XMLisp/sources/XLUI/examples/3D/agents/Apocalypse Now.lisp
@@ -0,0 +1,72 @@
+;;; Lisp User Interface: Chopper
+;;; Alexander Repenning
+;;; 11/24/02 OpenGL for MCL 1.0
+;;; 10/06/05 OpenGL for MCL 1.6
+;;; 05/07/09 CCL
+
+(in-package :xlui)
+
+
+;; window
+
+(defclass GAME-WINDOW (application-window)
+  ())
+
+;; agents
+
+(defclass ROTOR (tile) 
+  ((rotor-speed :accessor rotor-speed :initform 90.0 :type float :documentation "degrees/sec")))
+
+
+(defmethod ANIMATE ((Self rotor) Time)
+  (setf (roll Self) (mod (+ (roll Self) (* Time (rotor-speed Self))) 360.0)))
+
+
+(defclass CHOPPER (group)
+  ())
+
+
+(defmethod ANIMATE ((Self chopper) dt)
+  ;; http://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
+  (let* ((Alpha (/ dt (+ dt 5.0)))
+         (1-Alpha (- 1 Alpha)))
+    (setf (x Self) (+ (* Alpha (- (random 10.0) 5.0)) (* 1-Alpha (x self))))
+    (setf (y Self) (+ (* Alpha (- (random 10.0) 5.0)) (* 1-Alpha (y self))))
+    (setf (z Self) (+ (* Alpha (- (random 10.0) 5.0)) (* 1-Alpha (z self))))
+    (setf (heading Self) (+ (* Alpha (- (random 100.0) 50.0)) (* 1-Alpha (heading Self))))
+    (setf (pitch Self) (+ (* Alpha (- (random 100.0) 50.0)) (* 1-Alpha (pitch Self))))
+    (setf (roll Self) (+ (* Alpha (- (random 100.0) 50.0)) (* 1-Alpha (roll Self))))
+    (call-next-method)))
+
+
+;; Actions
+
+(defmethod START ((Window game-window) (button bevel-button))
+  (start-animation (view-named Window "war")))
+
+
+(defmethod STOP ((Window game-window) (button bevel-button))
+  (stop-animation (view-named Window "war")))
+
+
+<game-window title="Choppers" track-mouse="true" margin="0">
+  <column align="stretch" valign="stretch">
+    <agent-3d-view name="war" vflex="1">
+      <chopper>
+        <group pitch="-90" x="-1.0">
+          <rotor rotor-speed="360.0" width="5.0" height="0.2" x-turn="2.5" y-turn="0.1" texture="metal1.jpg"/>
+        </group>
+        <cube x="1.0" y="-1.3" z="-0.5" texture="metal1.jpg"/>
+        <cylinder x="2.0" y="-0.8" heading="90" base-radius="0.3" top-radius="0.1" height="2.0" texture="metal1.jpg"/>
+        <rotor rotor-speed="720.0" x="3.5" y="-0.8" z="0.2"  width="1.0" height="0.1" x-turn="0.5" y-turn="0.05" texture="metal1.jpg"/>
+      </chopper>
+      <sky-dome pitch="90"/>
+    </agent-3d-view>
+    <row minimize="vertical" align="center">
+      <bevel-button text="start" action="start" width="55"/>
+      <bevel-button text="stop" action="stop" width="55"/>
+    </row>
+  </column>
+</game-window>
+
+
---
+++ XMLisp/sources/XMLisp-init.lisp
@@ -0,0 +1,156 @@
+;; Start XMlisp
+;; 04/24/09 Alexander Repenning
+;; 5/20/09 + speech
+;; for CCL 1.3
+
+(in-package :cl-user)
+
+;***************** Settings
+
+;; edit to point to root folder containing /sources  /resources  etc.
+(setf (logical-pathname-translations "lui")
+      '(("**;*.*" "home:documents;products;XMLisp;**;")))
+
+
+(setq gui::*paren-highlight-background-color*
+      (#/retain (#/colorWithCalibratedRed:green:blue:alpha: ns:ns-color 0.9 0.8 0.8 1.0)))
+
+;; Load
+
+;; IDE
+
+(load "lui:sources;IDE;specific;Mac CCL;anticipat-symbol-complete")
+(load "lui:sources;IDE;specific;Mac CCL;ns timer")
+(load "lui:sources;IDE;specific;Mac CCL;hemlock extensions")
+
+
+;; Open
+
+(ed "lui:sources;XLUI;examples;2D;test case examples.lisp")
+;; (ed "lui:sources;XLUI;examples;AgentCubes Design.lisp")
+
+(ed "lui:sources;Lisp User Interface;LUI.lisp")
+(ed "lui:sources;Lisp User Interface;specific;Mac CCL;LUI Cocoa.lisp")
+
+
+;;****** OpenGL
+
+(defpackage :OPENGL
+  (:use :common-lisp))
+
+(load "lui:sources;OpenGL;specific;Mac CCL;OpenGL-interface")
+
+;;****** LUI
+
+(defpackage :LUI
+  (:use :common-lisp :ccl :opengl)
+  (:export 
+   "MOUSE-EVENT" "TRACK-MOUSE" "VIEW-MOUSE-MOVED-EVENT-HANDLER" "VIEW-LEFT-MOUSE-DOWN-EVENT-HANDLER"
+   "COMMAND-KEY-P" "SHIFT-KEY-P" "ALT-KEY-P" "CONTROL-KEY-P"
+   "WINDOW" "VIEW" "X" "Y" "WIDTH" "HEIGHT" "SHOW" "SHOW-AND-RUN-MODAL" "STOP-MODAL" "CANCEL-MODAL" "HIDE" 
+   "SCROLL-VIEW" "HAS-HORIZONTAL-SCROLLER" "HAS-VERTICAL-SCROLLER"
+   "SET-SIZE" "SET-POSITION" "DISPLAY" "DRAW" "INIT" "CLEAR-BACKGROUND" "RECURSIVE-MAP-SUBVIEWS"
+   "RECTANGLE-VIEW" "SET-COLOR"
+   "ZOOMABLE" "MINIMIZABLE" "RESIZABLE" "CLOSEABLE" "TITLE"
+   "SCREEN-WIDTH" "SCREEN-HEIGHT" "CONTROL" "VALUE" "INITIALIZE-EVENT-HANDLING" "SIZE-CHANGED-EVENT-HANDLER"
+   "ACTION" "SUBVIEWS" "DO-SUBVIEWS" "MAP-SUBVIEWS" "ADD-SUBVIEW" "ADD-SUBVIEWS" "SET-FRAME"
+   "BUTTON-CONTROL" "DEFAULT-BUTTON"
+   "BEVEL-BUTTON-CONTROL" "SLIDER-CONTROL" "TICK-MARKS" "MIN-VALUE" "MAX-VALUE"
+   "LABEL-CONTROL" "TEXT" "ALIGN"
+   "EDITABLE-TEXT-CONTROL"
+   "VALUE"
+   "IMAGE-CONTROL" "SRC" "FILE"
+   "WEB-BROWSER-CONTROL" "URL"
+   "OPENGL-VIEW" "FRAME-RATE" "ANIMATE" "START-ANIMATION" "STOP-ANIMATION" "FULL-SCENE-ANTI-ALIASING"
+    "USE-TEXTURE"
+   "CAMERA" "AIM-CAMERA" "WITH-GLCONTEXT" "RENDER-FOR-SELECTION-MODE"
+   "NATIVE-PATH"
+   ;; Dialogs
+   "STANDARD-ALERT-DIALOG"
+   ;; Multimedia
+   "PLAY-SOUND"
+   "SYNTHESIZER" "SPEAK" "WILL-SPEAK-WORD" "WILL-SPEAK-PHONEME" "DID-FINISH-SPEAKING" "AVAILABLE-VOICES"
+   ))
+
+
+(defun LUI::NATIVE-PATH (Directory-Name File-Name) "
+  in: Directory-Name logical-pathname-string, e.g., ''lui:resources;textures;''
+      File-Name string.
+  out: Native-Path-String
+  Create a native, OS specific, path from a platform independend URL style path"
+  (format nil "~A~A" (truename Directory-Name) File-Name))
+
+
+
+
+;; frameworks
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ccl:use-interface-dir :GL)
+  (open-shared-library "/System/Library/Frameworks/OpenGL.framework/OpenGL"))
+
+
+;; files
+
+(load "lui:sources;Lisp User Interface;specific;Mac CCL;memory")
+(load "lui:sources;Lisp User Interface;LUI")
+(load "lui:sources;Lisp User Interface;OpenGL-view")
+(load "lui:sources;Lisp User Interface;specific;Mac CCL;LUI Cocoa")
+(load "lui:sources;Lisp User Interface;specific;Mac CCL;image-import")
+(load "lui:sources;Lisp User Interface;specific;Mac CCL;OpenGL-view Cocoa")
+(load "lui:sources;Lisp User Interface;Camera")
+(load "lui:sources;Lisp User Interface;specific;Mac CCL;speech")
+
+;****** XMLisp
+
+(defpackage :XML
+  (:export "XML-SERIALIZER"))
+
+(load "lui:sources;XMLisp;XMLisp")
+
+
+;****** XLUI
+
+(defpackage :XLUI
+  (:use :common-lisp :XML :LUI :opengl))
+
+(setq xml::*xmlisp-packages* (list (find-package :xlui) (find-package :xml)))
+
+
+(load "lui:sources;XLUI;xml-layout")
+(load "lui:sources;XLUI;controls")      ;; LOTS of undefined functions still!
+(load "lui:sources;XLUI;application-window")
+(load "lui:sources;XLUI;dialog-window")
+(load "lui:sources;XLUI;agent-3D")
+
+
+;******** Multimedia
+
+(defpackage :sound
+  (:use :common-lisp :ccl)
+  (:export "PLAY-SOUND"))
+
+
+;;*************** Build functions
+
+(defun ccl::CCL-CONTENTS-DIRECTORY ()
+  (let* ((heap-image-path (ccl::%realpath (ccl::heap-image-name))))
+    (make-pathname :directory (butlast (pathname-directory heap-image-path))
+                   :device (pathname-device heap-image-path))))
+
+
+(defun BUILD-XMLISP ()
+  (require :build-application)
+  ;; load a different init file
+  (defmethod ccl::application-init-file ((app ccl::lisp-development-system))
+    '("home:xmlisp-init" "home:\\.xmlisp-init"))
+  ;; create LUI host pointing to application bundle
+  (defmethod ccl::toplevel-function  ((a ccl::application) init-file)
+    (declare (ignore init-file))
+    (setf (logical-pathname-translations "lui")
+      `(("**;*.*" ,(merge-pathnames "**/*.*" (ccl-contents-directory))))))
+  (ccl::build-application 
+   :name "XMLisp"
+   :directory (format nil "~ADesktop/" (user-homedir-pathname))))
+
+
