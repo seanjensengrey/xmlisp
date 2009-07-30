0bcb7a70281dd5fbe9a3171eabba39f157c433a4
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2009-07-30 03:43:15 +0000

    cube is draggable, fix sky dome rotation

--- XMLisp/sources/XLUI/examples/3D/agents/Apocalypse Now.lisp
+++ XMLisp/sources/XLUI/examples/3D/agents/Apocalypse Now.lisp
@@ -61,11 +61,11 @@
         <group pitch="-90" x="-1.0">
           <rotor rotor-speed="360.0" width="5.0" height="0.2" x-turn="2.5" y-turn="0.1" texture="metal1.jpg"/>
         </group>
-        <cube x="1.0" y="-1.3" z="-0.5" texture="metal1.jpg"/>
+        <cube draggable="true" x="1.0" y="-1.3" z="-0.5" texture="metal1.jpg"/>
         <cylinder x="2.0" y="-0.8" heading="90" base-radius="0.3" top-radius="0.1" height="2.0" texture="metal1.jpg"/>
         <rotor rotor-speed="720.0" x="3.5" y="-0.8" z="0.2"  width="1.0" height="0.1" x-turn="0.5" y-turn="0.05" texture="metal1.jpg"/>
       </chopper>
-      <sky-dome pitch="90"/>
+      <sky-dome pitch="-90"/>
     </agent-3d-view>
     <row minimize="vertical" align="center">
       <bevel-button text="start" action="start-action" width="55"/>
