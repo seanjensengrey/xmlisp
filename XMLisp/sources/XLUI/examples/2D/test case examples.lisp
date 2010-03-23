b664502fa1909c30a20ee2c32d368128c45897cb
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-03-23 21:24:05 +0000

    Moved pop ups examples into their own group

--- XMLisp/sources/XLUI/examples/2D/test case examples.lisp
+++ XMLisp/sources/XLUI/examples/2D/test case examples.lisp
@@ -278,29 +278,6 @@ The lawyer said, 'I don't mind all this attention, but what makes me so special?
 St. Peter replied, 'Well, I've added up all the hours for which you billed your clients, and by my calculation you must be about 193 years old!'"/>
 </application-window> 
 
-;; Popup Tests
-
-
-(defmethod show-string-menu ((w application-window) (Button button))
-  (print (show-string-popup w '("hello" "hello2" "hello3"))))
-
-;;General Pop Up Test
-(print (display-pop-up '("direction_north_west.png" "direction_north_west.png"   "direction_north_east.png"  ))) 
-
-;; Directional Picker Test
-
-(defparameter *Pop-Up-Menu* (make-instance 'directional-pop-up-image-menu :name-of-selection  "direction_north_west"))
-
-
-
-
-<application-window title="Currency Converter" width="300" height="180">
-  <column align="stretch" valign="stretch" padding="9">
-    <row align="stretch" minimize="vertical" valign="bottom">
-      <button text="make-event" action="pop-up-image-menu-now" width="100" default-button="true"/>
-    </row>
-  </column>
-</application-window>
 
 ;; Editable-Text
 
@@ -343,6 +320,28 @@ St. Peter replied, 'Well, I've added up all the hours for which you billed your 
   </column>
 </application-window>
 
+;;********************************************
+;;*           Pop ups                        *
+;;********************************************
+
+(defmethod SHOW-STRING-MENU ((w application-window) (Button button))
+  (print (show-string-popup w '("hello" "hello2" "hello3"))))
+
+
+;;General Pop Up Test
+(print (display-pop-up '("direction_north_west.png" "direction_north_west.png"   "direction_north_east.png")))
+
+;; Directional Picker Test
+(defparameter *Pop-Up-Menu* (make-instance 'directional-pop-up-image-menu :name-of-selection  "direction_north_west"))
+
+
+<application-window title="Currency Converter" width="300" height="180">
+  <column align="stretch" valign="stretch" padding="9">
+    <row align="stretch" minimize="vertical" valign="bottom">
+      <button text="make-event" action="pop-up-image-menu-now" width="100" default-button="true"/>
+    </row>
+  </column>
+</application-window>
 
 ;;****************************************************
 ;; IMAGES  (assumed to be in LUI:resources;images;)  *
