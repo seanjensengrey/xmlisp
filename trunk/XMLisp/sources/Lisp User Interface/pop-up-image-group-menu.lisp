3a6d33df05c6733c351bf3b12ca4875d40a1233c
pokermike2012 <pokermike2012@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-05-05 17:42:30 +0000

    Readded preview

--- trunk/XMLisp/sources/Lisp User Interface/pop-up-image-group-menu.lisp
+++ trunk/XMLisp/sources/Lisp User Interface/pop-up-image-group-menu.lisp
@@ -5,9 +5,6 @@
 ;;*********************************
 
 
-;(export '(pop-up-image-group-menu))
-
-
 (defclass POP-UP-IMAGE-GROUP-MENU ()
   ((native-window :accessor native-window :initform nil)
    (native-view :accessor native-view :initform nil)
@@ -17,32 +14,34 @@
    (height :accessor height :initform 300 :initarg :height :documentation "height in pixels")
    (image-height :accessor image-height :initform 20 :documentation "height of image")
    (image-width :accessor image-width :initform 20 :documentation "height of image")
+   (shape-text-box :accessor shape-text-box :initform nil :documentation "a reference to the text field which is used to display the selcted shape")
+   (shape-text-box-height :accessor shape-text-box-height :initform 20)
+   (image-preview-view :accessor image-preview-view :initform nil)
+   (image-preview-height :accessor image-preview-height :initform 32)
    (window-height :accessor window-height :initform 200 :documentation "height of window")
    (image-file-extension :accessor image-file-extension :initform "png" :initarg :image-file-extension :documentation "the extension of the image file")
    (image-pathname :accessor image-pathname :initform "lui:resources;buttons;" :initarg :image-pathname :documentation "the path to where the image files are stored")
+   (image-border-thickness :accessor image-border-thickness :initform 2 :documentation "the thickness of the border of each image in the menu")
    (label-width :accessor label-width :initform 0.0 :documentation "Width of the labels")
    (index-of-selection :accessor index-of-selection :initform nil :documentation "the index of the selected item")
    (name-of-selection :accessor name-of-selection :initform nil :initarg :name-of-selection :documentation "the name of the selected item")
    (window-offset-x :accessor window-offset-x :initform 0 :documentation "the X offset needed to display the window in the propper location")
    (window-offset-y :accessor window-offset-y :initform 0 :documentation "the Y offset needed to display the window in the propper location")
-   (images :accessor images :initform nil :initarg :images :documentation "A nested list of groups with images"))
+   (images :accessor images :initform nil :initarg :images :documentation "A nested list of groups with images")
+   )
   (:documentation "This class will pop up an an image menu that is seperated into groups.  It displays the given list of strings sorted by group."))
 
 
 (defmethod INITIALIZE-INSTANCE :after  ((Self pop-up-image-group-menu) &rest Initargs)
   (declare (ignore initargs))
-  (setf (window-height self) (* (image-height self) (length (image-names self))))
+  (setf (window-height self) (+ (image-preview-height self)(shape-text-box-height self) (* (image-height self) (length (image-names self)))))
   (setf (width self) (get-window-width self)))
 
 
-(defclass INDEXED-IMAGE-VIEW (ns:ns-image-view)
-  ((index :accessor index :initform 200  :initarg :index :documentation "index of button")
-   (lui-superview :accessor lui-superview :initform nil :initarg :lui-superview))
-  (:metaclass ns:+ns-object
-	      :documentation "Index Button"))
 ;---------------------------------
 ; Specification                  |
 ;________________________________
+
 
 (defgeneric DISPLAY-POP-UP-MENU (pop-up-image-group-menu &key x y)
   (:documentation "Pops up the menu and returns the value of the selected image"))
@@ -50,6 +49,7 @@
 
 (defgeneric IMAGE-NAMES (pop-up-image-group-menu)
   (:documentation "Returns the list of groups and their images that will be used by the menu"))
+
 
 ;---------------------------------
 ; Implementation                  |
@@ -78,6 +78,11 @@
     ("Selection" ("magic-wand-button" "arrow-button" "select-ellipse-button"))
     ("Nagivation" ("zoom-button" "pan-button" "rotate-button"))))
 
+
+(defmethod SIZE-CHANGED-EVENT-HANDLER ((Self tool-pop-up-image-group-menu) Width Height)
+  (declare (ignore Width Height))
+  ;; nothing
+  )
 
 (defparameter *Pop-Up-Menu* (make-instance 'tool-pop-up-image-group-menu) )
 
