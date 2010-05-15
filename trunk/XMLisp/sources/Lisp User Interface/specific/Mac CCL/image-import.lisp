aa6b8ddf6aba974e603e3e0f6d364f59752d879c
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-05-15 00:05:16 +0000

    retain nsImageRepresentation to avoid strange issues when loading inflatable-icons -> it appears as if nsImageRepresentation would get garbage collected and if so would deallocate the memory associated with the image loaded -> BAD

--- trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/image-import.lisp
+++ trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/image-import.lisp
@@ -91,7 +91,7 @@
   Create an image buffer from <Filename>
   - File must be 32 bit ARGB compatible, e.g., .png with mask or 24 bit RGB."
   (when Verbose (format t "CREATE-IMAGE-FROM-FILE: ~A~%" Filename))
-  (let* ((Image-Representation (ns-image-rep-from-file (native-string (namestring (translate-logical-pathname Filename))))))
+  (let* ((Image-Representation (#/retain (ns-image-rep-from-file (native-string (namestring (translate-logical-pathname Filename)))))))
     ;; should massage data: GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV for best performance
     ;; http://developer.apple.com/documentation/graphicsimaging/Conceptual/OpenGL-MacProgGuide/opengl_texturedata/opengl_texturedata.html
     (when (%null-ptr-p Image-Representation)
