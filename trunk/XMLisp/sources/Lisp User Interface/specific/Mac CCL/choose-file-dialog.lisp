c410ac4afe055bc04dde74990da12ec14c620a89
pokermike2012 <pokermike2012@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-05-19 22:25:39 +0000

    Improved choose-file-dialos, added select-item method to string-list and changed badged-image-group-list-manager to allow changing file source for an item or groups images.

--- trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/choose-file-dialog.lisp
+++ trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/choose-file-dialog.lisp
@@ -4,26 +4,101 @@
 (in-package :lui)
 
 
-(defun choose-file-dialog ()
+(defun choose-file-dialog (&key 
+                           (directory "cclDirectory") ;
+                           ;mac-file-type
+                           ;mac-file-creator
+                           (button-string "Open") ;;
+                           ;(cancel-button-string "Cancel")
+                           (window-title "Choose a File") ;;
+                           (allow-multiple-files nil)
+                           prompt ;;
+                           (window-position (ns:make-ns-point 100 100))
+                           file-type-string-list ;;
+                           )
   (let* ((panel (#/openPanel ns:ns-open-panel))
 	 (dc (#/sharedUserDefaultsController ns:ns-user-defaults-controller))
          (values (#/values dc))
-	 (key #@"cclDirectory"))
-    (#/setAllowsMultipleSelection: panel nil)
+	 ;(key #@"cclDirectory")
+         (file-types +null-ptr+))
+    (when file-type-string-list
+      (dotimes (i (length file-type-string-list))
+        (unless (equal (type-of (elt file-type-string-list i)) (type-of (native-string "string")))
+          (progn
+            (setf (elt file-type-string-list i) (native-string (elt file-type-string-list i))))))
+      (setf file-types (gui::ns-array-from-list file-type-string-list)))
+    (when button-string
+      (setf button-string (ccl::%make-nsstring button-string))
+      (#/setPrompt: panel button-string))
+    (when window-title
+      (setf window-title (ccl::%make-nsstring window-title))
+      (#/setTitle: panel window-title))
+    (when prompt
+      (setf prompt (ccl::%make-nsstring prompt))
+      (#/setMessage: panel  prompt))
+    (#/setAllowsMultipleSelection: panel allow-multiple-files)
     (#/setCanChooseDirectories: panel nil)
     (#/setCanChooseFiles: panel t)
+    (#/setFrameOrigin: panel window-position)
+    (#/setAllowedFileTypes:  panel (#/arrayWithObject: ns:ns-array (native-string "lisp")))
+    (#/makeKeyAndOrderFront: panel +null-ptr+)
     (when (eql (#/runModalForDirectory:file:types: panel
-						   (#/valueForKey: values key)
+						   (#/valueForKey: values (native-string directory))
+						   +null-ptr+
+						   file-types)
+	       #$NSOKButton)
+      (pathname (ccl::lisp-string-from-nsstring (#/objectAtIndex: (#/filenames Panel) 0))))))
+
+
+(defun choose-directory-dialog (&key 
+                           (directory "cclDirectory") ;
+                           ;mac-file-type
+                           ;mac-file-creator
+                           (button-string "Open") ;;
+                           ;(cancel-button-string "Cancel")
+                           (window-title "Choose a File") ;;
+                           prompt ;;
+                           (window-position (ns:make-ns-point 100 100)))
+  (let* ((panel (#/openPanel ns:ns-open-panel))
+	 (dc (#/sharedUserDefaultsController ns:ns-user-defaults-controller))
+         (values (#/values dc))
+	 ;(key #@"cclDirectory")
+         )
+    (when button-string
+      (setf button-string (ccl::%make-nsstring button-string))
+      (#/setPrompt: panel button-string))
+    (when window-title
+      (setf window-title (ccl::%make-nsstring window-title))
+      (#/setTitle: panel window-title))
+    (when prompt
+      (setf prompt (ccl::%make-nsstring prompt))
+      (#/setMessage: panel  prompt))
+    (#/setAllowsMultipleSelection: panel nil)
+    (#/setCanChooseDirectories: panel t)
+    (#/setCanChooseFiles: panel nil)
+    (#/setFrameOrigin: panel window-position)
+    (#/setAllowedFileTypes:  panel (#/arrayWithObject: ns:ns-array (native-string "lisp")))
+    (#/makeKeyAndOrderFront: panel +null-ptr+)
+    (when (eql (#/runModalForDirectory:file:types: panel
+						   (#/valueForKey: values (native-string directory))
 						   +null-ptr+
 						   +null-ptr+)
 	       #$NSOKButton)
-      (ccl::lisp-string-from-nsstring (#/objectAtIndex: (#/filenames Panel) 0)))))
+      (pathname (ccl::lisp-string-from-nsstring (#/objectAtIndex: (#/filenames Panel) 0))))))
 
+
+
+
+(defun PARSE-LISP-PATH-FROM-STIRNG (string)
+  (count "/"  string :test #'string=)
+  )
 
 
 #| Examples:
 
-(choose-file-dialog)
+(choose-file-dialog  :file-type-string-list (list  "lisp"   "window") )
+(choose-directory-dialog)
+
 
 |#
 
