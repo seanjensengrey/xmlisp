af6cb0f699191d38604ecdf9d140ed1e9abbd8c6
pokermike2012 <pokermike2012@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-05-20 17:16:31 +0000

    added functionality to change text of cancel button

--- XMLisp/sources/Lisp User Interface/specific/Mac CCL/choose-file-dialog.lisp
+++ XMLisp/sources/Lisp User Interface/specific/Mac CCL/choose-file-dialog.lisp
@@ -3,30 +3,48 @@
 
 (in-package :lui)
 
+(defun ns-array-from-list (list)
+  "The list must be an array of ns-objects"
+  (let ((nsa (#/array ns:ns-array)))
+    (dolist (list-item list)
+      (setf nsa (#/arrayByAddingObject: nsa list-item)))
+    nsa))
+
 
 (defun choose-file-dialog (&key 
                            (directory "cclDirectory") ;
                            ;mac-file-type
                            ;mac-file-creator
-                           (button-string "Open") ;;
-                           ;(cancel-button-string "Cancel")
-                           (window-title "Choose a File") ;;
+                           (button-string "Open") 
+                           cancel-button-string 
+                           (window-title "Choose a File") 
                            (allow-multiple-files nil)
-                           prompt ;;
+                           prompt 
                            (window-position (ns:make-ns-point 100 100))
-                           file-type-string-list ;;
+                           file-type-string-list 
                            )
   (let* ((panel (#/openPanel ns:ns-open-panel))
 	 (dc (#/sharedUserDefaultsController ns:ns-user-defaults-controller))
          (values (#/values dc))
 	 ;(key #@"cclDirectory")
          (file-types +null-ptr+))
+    ;; Kind of nasty code just to change the title of the cancel button, need to go through two layers of subviews to find the button and change its title.  
+    (if cancel-button-string
+      (let ((subviews (gui::list-from-ns-array (#/subviews (#/contentView panel)))))
+        (dolist (subview subviews)
+          (if (equal (type-of subview) 'NS:NS-VIEW)
+            (let ((subviews2 (gui::list-from-ns-array (#/subviews subview))))
+              (dolist (subview2 subviews2)
+                (if (equal (type-of subview2) 'NS:NS-BUTTON )
+                  ;(print (#/title subview2))
+                  (if (equal (#/title subview2) (native-string "Cancel"))
+                    (#/setTitle: subview2 (native-string cancel-button-string))))))))))
     (when file-type-string-list
       (dotimes (i (length file-type-string-list))
         (unless (equal (type-of (elt file-type-string-list i)) (type-of (native-string "string")))
           (progn
             (setf (elt file-type-string-list i) (native-string (elt file-type-string-list i))))))
-      (setf file-types (gui::ns-array-from-list file-type-string-list)))
+      (setf file-types (ns-array-from-list file-type-string-list)))
     (when button-string
       (setf button-string (ccl::%make-nsstring button-string))
       (#/setPrompt: panel button-string))
@@ -55,7 +73,7 @@
                            ;mac-file-type
                            ;mac-file-creator
                            (button-string "Open") ;;
-                           ;(cancel-button-string "Cancel")
+                           cancel-button-string 
                            (window-title "Choose a File") ;;
                            prompt ;;
                            (window-position (ns:make-ns-point 100 100)))
@@ -64,6 +82,15 @@
          (values (#/values dc))
 	 ;(key #@"cclDirectory")
          )
+    (if cancel-button-string
+      (let ((subviews (gui::list-from-ns-array (#/subviews (#/contentView panel)))))
+        (dolist (subview subviews)
+          (if (equal (type-of subview) 'NS:NS-VIEW)
+            (let ((subviews2 (gui::list-from-ns-array (#/subviews subview))))
+              (dolist (subview2 subviews2)
+                (if (equal (type-of subview2) 'NS:NS-BUTTON )
+                  (if (equal (#/title subview2) (native-string "Cancel"))
+                    (#/setTitle: subview2 (native-string cancel-button-string))))))))))
     (when button-string
       (setf button-string (ccl::%make-nsstring button-string))
       (#/setPrompt: panel button-string))
