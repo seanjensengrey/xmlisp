7072b3447dfd30fe1f7a43be399a511be29e974a
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-06-25 00:44:22 +0000

    added: get-color-from-user.lisp as modal dialog to get color

---
+++ XMLisp/sources/Lisp User Interface/specific/Mac CCL/get-color-from-user.lisp
@@ -0,0 +1,17 @@
+(in-package :lui)
+
+(defun GET-COLOR-FROM-USER ()
+  (let ((Color (easygui::user-pick-color)))
+        (rlet ((r #>CGFloat)
+               (g #>CGFloat)
+               (b #>CGFloat)
+               (a #>CGFloat))
+          (#/getRed:green:blue:alpha: Color r g b a)         
+          `(,(truncate (* (lui::pref r #>CGFloat) 255)) ,(truncate (* (lui::pref g #>CGFloat) 255)) ,(truncate (* (lui::pref b #>CGFloat) 255)) ,(truncate (* (lui::pref a #>CGFloat) 255)) ))))
+
+
+#| Examples:
+
+(get-color-from-user)
+
+|#
