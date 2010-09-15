afbc329996ed3fafa8fb06f8005e523d468e733e
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-09-15 21:34:08 +0000

    GET-COLOR-FROM-USER: exported from lui, :shows-alpha keyword

--- trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/get-color-from-user.lisp
+++ trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/get-color-from-user.lisp
@@ -1,19 +1,32 @@
 (in-package :lui)
 
-(defun GET-COLOR-FROM-USER ()
-  #-cocotron 
-  (let ((Color (easygui::user-pick-color)))
-        (rlet ((r #>CGFloat)
-               (g #>CGFloat)
-               (b #>CGFloat)
-               (a #>CGFloat))
-          (#/getRed:green:blue:alpha: Color r g b a)         
-          `(,(truncate (* (lui::pref r #>CGFloat) 255)) ,(truncate (* (lui::pref g #>CGFloat) 255)) ,(truncate (* (lui::pref b #>CGFloat) 255)) ,(truncate (* (lui::pref a #>CGFloat) 255)) )))
-  )
+
+(export '(get-color-from-user))
+
+(defun GET-COLOR-FROM-USER (&key (Shows-Alpha t)) "
+  Make user pick an RGBA value"
+  #-cocotron
+  (progn
+    (#/setShowsAlpha: (#/sharedColorPanel ns:ns-color-panel) Shows-Alpha)
+    (let ((Color (easygui::user-pick-color)))
+      (rlet ((r #>CGFloat)
+             (g #>CGFloat)
+             (b #>CGFloat)
+             (a #>CGFloat))
+        (#/getRed:green:blue:alpha: Color r g b a)
+        (values 
+         (float (lui::pref r #>CGFloat) 0.0)
+         (float (lui::pref g #>CGFloat) 0.0)
+         (float (lui::pref b #>CGFloat) 0.0)
+         (float (lui::pref a #>CGFloat) 0.0)))))
+  #+cocotron
+  ;; need to implement a real color picker
+  (values 1.0 1.0 1.0 1.0))
 
 
 #| Examples:
 
 (get-color-from-user)
+(get-color-from-user :shows-alpha nil)
 
 |#
