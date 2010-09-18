24f573b5f2fe235501b76df7bc5c2032026fe138
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-09-18 00:55:04 +0000

    GET-COLOR-FROM-USER has Red, Green, Blue, Alpha keyword args

--- trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/get-color-from-user.lisp
+++ trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/get-color-from-user.lisp
@@ -3,11 +3,14 @@
 
 (export '(get-color-from-user))
 
-(defun GET-COLOR-FROM-USER (&key (Shows-Alpha t)) "
+(defun GET-COLOR-FROM-USER (&key (Shows-Alpha t) (Red 1.0) (Green 0.0) (Blue 0.0) (Alpha 1.0)) "
   Make user pick an RGBA value"
   #-cocotron
   (progn
     (#/setShowsAlpha: (#/sharedColorPanel ns:ns-color-panel) Shows-Alpha)
+    (#/setColor: 
+     (#/sharedColorPanel ns:ns-color-panel)
+     (#/colorWithCalibratedRed:green:blue:alpha: ns:ns-color Red Green Blue Alpha))
     (let ((Color (easygui::user-pick-color)))
       (rlet ((r #>CGFloat)
              (g #>CGFloat)
