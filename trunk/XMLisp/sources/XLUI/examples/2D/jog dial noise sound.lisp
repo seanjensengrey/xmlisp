03fc46910e779522abb30a3dcedb0d77aa83c84a
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-10-13 23:11:52 +0000

    new example: jog dial noise sound

---
+++ trunk/XMLisp/sources/XLUI/examples/2D/jog dial noise sound.lisp
@@ -0,0 +1,42 @@
+;; Simple example of a jog slider
+;; Alexander Repenning
+;; Move the slider and notice:
+;;  - control action keeps getting called even if knob is not moving anymore. Frequency can be controlled
+;;  - after mouse up slider snaps back to stop-value
+
+
+(in-package :lui)
+
+
+(defclass INFLATION-JOG-SLIDER (jog-slider-control)
+  ())
+
+
+(defmethod START-JOG ((Self inflation-jog-slider))
+  (call-next-method)
+  (play-sound "whiteNoise.mp3" :loops t))
+
+
+(defmethod STOP-JOG ((Self inflation-jog-slider))
+  (call-next-method)
+  (stop-sound "whiteNoise.mp3"))
+
+
+(defclass JOG-NOISE-WINDOW (window)
+  ()
+  (:default-initargs
+    :width 400
+    :height 200))
+
+
+(defmethod INITIALIZE-INSTANCE :after ((Self jog-noise-window) &rest Args)
+  (declare (ignore Args))
+  (add-subviews Self (make-instance 'inflation-jog-slider :width 300 :x 50 :action 'JOG :min-value -1.0 :max-value 1.0 :stop-value 0.0)))
+
+
+(defmethod JOG ((Self jog-noise-window) (Slider inflation-jog-slider))
+  (set-volume "whiteNoise.mp3" (* 0.1 (abs (value Slider))))
+  (format t "~%action: jog value is ~A" (value Slider)))
+
+
+(defparameter *Jog-Window* (make-instance 'jog-noise-window))
