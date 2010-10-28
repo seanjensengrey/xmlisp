aa7bfabee827af5333faab48ec3f302a751a1486
pokermike2012 <pokermike2012@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-10-28 16:27:19 +0000

    Added Image-Tools.lisp

---
+++ XMLisp/sources/XLUI/image-tools.lisp
@@ -0,0 +1,42 @@
+(in-package :xlui)
+
+(defun CONVERT-IMAGE-FILE (Source Destination &key (Width 32) (Height 32) (Depth 32) 
+                                     (image-format :png)
+                                     ) "
+  in:  Source Destination Pathname; 
+       &key Width fixnum default 32; Height fixnum default 32; Depth fixnum default 32; 
+       Image-Format can be :png :bmp :jpg :tiff
+  Create a new image file of different size and image-format."
+  ;; I still need to find a way to set the image depth.
+  (unless (probe-file source)
+    (error "Cannot covnert image file because ~A does not exist." source)
+    (return-from convert-image-file))
+  (ns:with-ns-size (Size Width Height)
+    (let* ((source-image (#/initWithContentsOfFile: (#/alloc ns:ns-image) (lui::native-string source)))
+           (resized-image (#/initWithSize: (#/alloc ns:ns-image) size))
+           (original-size (#/size source-image)))
+      (unless (and (equal width (NS:NS-SIZE-WIDTH original-size)) (equal height (NS:NS-SIZE-HEIGHT original-size)))
+        (#/lockFocus resized-image)
+        (#/drawInRect:fromRect:operation:fraction: 
+         source-image 
+         (ns::make-ns-rect 0 0 width height)
+         (ns::make-ns-rect 0 0 (NS:NS-SIZE-WIDTH original-size) (NS:NS-SIZE-HEIGHT original-size))
+         #$NSCompositeSourceOver
+         1.0)
+        (#/unlockFocus resized-image))
+      (#/writeToFile:atomically: 
+       (#/representationUsingType:properties: 
+        (#/initWithData: (#/alloc ns:ns-bitmap-image-rep) (#/TIFFRepresentation resized-image))  
+        (case image-format
+          (:png #$NSPNGFileType)
+          (:jpg #$NSJPEGFileType)
+          (:bmp #$NSBMPFileType)
+          (:tiff #$NSTIFFFileType))
+        ccl::+null-ptr+)
+       (lui::native-string Destination) 
+       #$YES))))
+
+#| Example:
+(convert-image-file "/Users/Mike/working copies/XMLisp svn/trunk/XMLisp/resources/images/logo_active.gif" "/Users/Mike/working copies/XMLisp svn/trunk/XMLisp/resources/images/logo_active.png" :width 64 :height 64)
+|#
+
