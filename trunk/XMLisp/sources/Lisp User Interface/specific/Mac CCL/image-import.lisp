49ef28ac02f0e09b84d8ae97853a823a21da3532
alex.repenning <alex.repenning@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2010-12-06 03:30:09 +0000

    Forced-Depth is no longer ignored - removed declaration to shut up compiler

--- trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/image-import.lisp
+++ trunk/XMLisp/sources/Lisp User Interface/specific/Mac CCL/image-import.lisp
@@ -103,7 +103,6 @@
        Width Height Forced-Depth int; Has-Alpha boolean.
   Create an image buffer from <Filename>
   - File must be 32 bit ARGB compatible, e.g., .png with mask or 24 bit RGB."
-  (declare (ignore Forced-Depth))
   (when Verbose (format t "CREATE-IMAGE-FROM-FILE: ~A~%" Filename))
   (let* ((Image-Representation (#/retain (ns-image-rep-from-file (native-string (namestring (translate-logical-pathname Filename)))))))
     ;; should massage data: GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV for best performance
