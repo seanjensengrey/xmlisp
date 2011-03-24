1b9c4759356d158a34e251552ca68f6605e8af0f
pokermike2012 <pokermike2012@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2011-03-24 00:39:48 +0000

    Added key parameter string-editor-type to get-string-from-user where the user could tell the function to use a different type of editable-text (a type-interactor perhaps?)

--- XMLisp/sources/XLUI/dialogs/get-string-from-user.lisp
+++ XMLisp/sources/XLUI/dialogs/get-string-from-user.lisp
@@ -5,12 +5,13 @@
 
 
 (defclass GET-STRING-FROM-USER-WINDOW (dialog-window)
-  ()
+  ((text-field :accessor text-field :initform nil ))
   (:documentation "Used to get a string from user"))
 
 
 (defmethod OK-ACTION ((Window get-string-from-user-window) (Button button))
-  (stop-modal Window (value (view-named Window "text"))))
+  (when (or (not (text-field window)) (lui::validate-final-text-value (text-field window) (value (text-field window))))
+    (stop-modal Window (value (view-named Window "text")))))
 
 
 (defmethod CANCEL-ACTION ((Window get-string-from-user-window) (Button button))
@@ -28,9 +29,10 @@
 
 (defun GET-STRING-FROM-USER (msg  &key (Initial-String "")
                                   (Yes-Text "OK")
-                                  (Cancel-Text "Cancel"))
+                                  (Cancel-Text "Cancel")
+                                  (string-editor-type 'editable-text-control))
   (let ((Alert (make-instance 'ns:ns-alert))
-        (tf (#/initWithFrame: (lui::native-view (make-instance 'editable-text-control))
+        (tf (#/initWithFrame: (lui::native-view (make-instance string-editor-type))
                               (ns:make-ns-rect 0 0 400 21))))
     (#/setStringValue: tf (ccl::%make-nsstring Initial-String))
     (#/setEditable: tf #$YES)
@@ -43,12 +45,17 @@
     (#/makeFirstResponder: (#/window alert) tf) 
     (when (equal (#/runModal alert) #$NSAlertSecondButtonReturn)
       (return-from get-string-from-user nil))
-    (ccl::lisp-string-from-nsstring (#/stringValue tf))))
-
+    (ccl::lisp-string-from-nsstring (#/stringValue tf))
+    (if (lui::validate-final-text-value (lui::lui-view tf) (Value (lui::lui-view tf)))
+      (ccl::lisp-string-from-nsstring (#/stringValue tf))
+      (get-string-from-user msg :Initial-String Initial-String :Yes-Text Yes-Text :Cancel-Text Cancel-Text :string-editor-type string-editor-type ))))
 
 
 #| Examples:
 
 (get-string-from-user "Who is your favorite actor?" :initial-string "Betty Boo")
 
+(get-string-from-user "What is your favorite number?" :initial-string "42" :string-editor-type 'number-type-control)
+
+(get-string-from-user "Who is your favorie Agent?" :initial-string "Bond" :string-editor-type 'agent-name-type-control)
 |#
