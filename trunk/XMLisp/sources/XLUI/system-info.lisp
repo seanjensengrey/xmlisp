d5b96b466f60a71253ab72cac3af92224ea3cfca
aiolida <aiolida@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2011-10-06 01:03:46 +0000

    getting system info - only works on macs

---
+++ trunk/XMLisp/sources/XLUI/system-info.lisp
@@ -0,0 +1,30 @@
+(in-package :xlui)
+
+(defun SYSTEM-INFO ()
+  (let ((Process-Info (#/alloc ns:ns-process-info)))
+    (values 
+     (case (#/operatingSystem Process-Info)
+       (1 "Windows NT")
+       (2 "Windows 95")
+       (3 "Solaris")
+       (4 "HPUX")
+       (5 "Mac OS X")
+       (6 "Sun OS")
+       (7 "OSF10")
+       (t "????"))
+     (ccl::lisp-string-from-nsstring (#/operatingSystemVersionString Process-Info))
+     (#/activeProcessorCount Process-Info)
+     (#/physicalMemory Process-Info))))
+
+#|
+(defun SYSTEM-INFO ()
+  (let ((Process-Info (#/alloc ns:ns-process-info)))
+    (values 
+     (#/operatingSystemName Process-Info)
+     (#/operatingSystem Process-Info)
+     (#/operatingSystemVersionString Process-Info)
+     (#/activeProcessorCount Process-Info)
+     (#/physicalMemory Process-Info))))
+|#
+
+; (system-info)
