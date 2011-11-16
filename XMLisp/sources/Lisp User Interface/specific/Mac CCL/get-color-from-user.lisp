fb0f4e0d204ccec3e968855cfead72f61d01bb54
pokermike2012 <pokermike2012@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2011-11-16 23:55:16 +0000

    get-color-from-user should pass red green blue and alpha values to the window color picker

--- XMLisp/sources/Lisp User Interface/specific/Mac CCL/get-color-from-user.lisp
+++ XMLisp/sources/Lisp User Interface/specific/Mac CCL/get-color-from-user.lisp
@@ -4,6 +4,7 @@
 
 (defun GET-COLOR-FROM-USER (&key (Shows-Alpha t) (Red 1.0) (Green 0.0) (Blue 0.0) (Alpha 1.0)) "
   Make user pick an RGBA value"
+  #+cocotron (declare (ftype function xlui::get-windows-color-picker))
   #-cocotron
   (progn
     (#/setShowsAlpha: (#/sharedColorPanel ns:ns-color-panel) Shows-Alpha)
@@ -23,7 +24,7 @@
          (float (lui::pref a #>CGFloat) 0.0)))))
   #+cocotron
   ;; need to implement a real color picker
-  (let ((color (xlui::get-windows-color-picker)))
+  (let ((color (xlui::get-windows-color-picker :red red :green green :blue blue :alpha alpha)))
     (when color
       (values (first color) (second color) (third color) (fourth color) ))))
 
