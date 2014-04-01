126c39a3aba402b8ecc8b8e3f3752e22646a05bf
pokermike2012 <pokermike2012@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2014-04-01 00:05:57 +0000

    removing print statements

--- trunk/XMLisp/sources/Lisp User Interface/Plot-view.lisp
+++ trunk/XMLisp/sources/Lisp User Interface/Plot-view.lisp
@@ -23,7 +23,7 @@
    ;(value-array :accessor value-array :initform (make-array 5 :fill-pointer 0 :adjustable t))
    (plot-color :accessor plot-color :initform (red-color-vector))
    (min-data-points-to-end-of-screen :accessor min-data-points-to-end-of-screen :initform 10)
-   (plot-list :accessor plot-list :initform () :documentation "This list of lists.  Each sublist contains first, the name of the representation, second, the color and third the vector of values and the fourth is ther starting time of the plot element")
+   (plot-lists :accessor plot-lists :initform () :documentation "This list of lists.  Each sublist contains first, the name of the representation, second, the color and third the vector of values and the fourth is ther starting time of the plot element")
    (y-padding-factor :accessor y-padding-factor :initform 1.1))
   (:documentation "A view for plotting values vs time")
   (:default-initargs 
@@ -48,22 +48,31 @@
   (glLoadIdentity))
 
 
+
 (defmethod ADD-PLOT-VALUE ((Self plot-view) value representing &key (color nil))
   (when (> value (max-value self))
     (setf (max-value self) value))
   (when (< value (min-value self))
     (setf (min-value self) value))
-  (let ((plot-list (find representing (plot-list self) :key #'first :test 'equal)))
+  (print (plot-lists self))
+  (let ((plot-list (find representing (plot-lists self) :key #'xlui::representing :test 'equal)))
     (unless plot-list   
       #+cocotron(display (xlui::view-named (window self) "export-button"))
       #+cocotron(display (xlui::view-named (window self) "reset-button"))
-      (setf (plot-list self) (append (plot-list self) (list (list  representing  color  (make-array 5 :fill-pointer 0 :adjustable t) (max-time self))))))
-    (when plot-list
-      (unless (equal color (second plot-list))
-        (setf (second plot-list) color))
-      (vector-push-extend  value (third plot-list))
-      (when (> (length (third plot-list)) (max-time self))
-        (setf (max-time self) (length (third plot-list))))))  
+      (setf (plot-lists self) (append (plot-lists self) (list (make-instance 'xlui::plot-list :representing representing :color color :plot-vector  (make-array 5 :fill-pointer 0 :adjustable t) :starting-time (max-time self)));(list (list  representing  color  (make-array 5 :fill-pointer 0 :adjustable t) (max-time self)))
+                                     )))
+      (when plot-list
+        (when (xlui::set-this-cycle-p plot-list)
+          (xlui::new-cycle (window self))
+          (error "Can't print to the same \"Representing\" in the same plot window twice in one simulation cycle. Please look at all of the \"plot to window\" actions in the selected agent before running this simulation agian.")
+         
+          )
+        (setf (xlui::set-this-cycle-p plot-list) t)
+        (unless (equal color (color plot-list))
+          (setf (color plot-list) color))
+        (vector-push-extend  value (xlui::plot-vector plot-list))
+        (when (> (length (xlui::plot-vector plot-list)) (max-time self))
+          (setf (max-time self) (length (xlui::plot-vector plot-list))))))  
   (display self))
 
 
@@ -94,20 +103,22 @@
   (glVertex3f 0.0 0.0 0.0)
   (glVertex3f (* 1.0 (if (> (max-time self) (width self)) (max-time self) (width self))) 0.0 0.0)
   (glEnd)
-  (dolist (plot-sublist (plot-list self))
-    (glcolor4f (first (second plot-sublist)) (second (second plot-sublist)) (third (second plot-sublist))  (fourth (second plot-sublist)))
+  (print "PLOT LISTS")
+  (print  (plot-lists self))
+  (dolist (plot-sublist (plot-lists self))
+    (glcolor4f (first (color plot-sublist)) (second (color plot-sublist)) (third (color plot-sublist))  (fourth (color plot-sublist)))
     (glBegin GL_LINE_STRIP)
     (let ((increment-value 1))
       (setf increment-value 1)
-      (dotimes (i (round (length (third plot-sublist)) increment-value))
-        (glVertex2f (+ (fourth plot-sublist) (* i  1.0)) (* 1.0 (aref (third plot-sublist) (* increment-value i)))))      
+      (dotimes (i (round (length (xlui::plot-vector plot-sublist)) increment-value))
+        (glVertex2f (+ (xlui::starting-time plot-sublist) (* i  1.0)) (* 1.0 (aref (xlui::plot-vector plot-sublist) (* increment-value i)))))      
       (glEnd))))
 
 (defmethod RESET ((self plot-view))
   (setf (lui::min-value self) 0.0d0)
   (setf (lui::max-value self) 0.0d0)
   (setf (lui::max-time self) 0.0)
-  (setf (lui::plot-list self) nil))
+  (setf (lui::plot-lists self) nil))
 
 
 
