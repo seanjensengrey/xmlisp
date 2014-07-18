becd94707dc189d63f2ef35e7e64935f0246f821
pokermike2012 <pokermike2012@305c0ee4-2d0a-11de-ba90-e1c2dc4d389a>
2014-07-18 16:21:31 +0000

    Added virtual logical-pathname for windows build script

--- XMLisp/sources/application-build-script.lisp
+++ XMLisp/sources/application-build-script.lisp
@@ -2,14 +2,26 @@
 
 (require 'cocoa)
 
+#-cocotron
 (require 'easygui)
 
 (load "ccl:cocoa-ide;defsystem.lisp")
 
-(load "home:working copies;XMLISP-RELEASE;XMLisp;sources;XMLisp-init")
+#-cocotron
+(load "home:working copies;Deployment;xmlisp;trunk;XMLisp;sources;XMLisp-init")
 
+#+cocotron
+(setf (logical-pathname-translations "virtual")
+`((,(make-pathname :host "virtual"
+                   :directory '(:absolute :wild-inferiors)
+                   :name :wild
+                   :type :wild
+                   :version :wild)
+   #p"x:/**/*.*")))
+
+#+cocotron 
+(load "virtual:Deployment;xmlisp;trunk;XMLisp;sources;XMLisp-init")
 ;; If this is uncomented the framte-rate will be unbound
 ;(setf lui::*Framerate-Ceilling* nil)
 
 (cl-user::build-agentcubes)
-
